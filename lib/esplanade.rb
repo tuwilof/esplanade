36e44783d9f64f8d3ebf43bb585465f2fd2b8f44
d.efimov <d.efimov@fun-box.ru>
2017-09-19 18:06:05 +0400

    delete errors

--- lib/esplanade.rb
+++ lib/esplanade.rb
@@ -2,10 +2,8 @@ require 'esplanade/middleware'
 require 'esplanade/configuration'
 require 'esplanade/request'
 require 'esplanade/request/body'
-require 'esplanade/request/error'
 require 'esplanade/response'
 require 'esplanade/response/body'
-require 'esplanade/response/error'
 require 'esplanade/railtie' if defined?(Rails)
 require 'tomograph'
 
