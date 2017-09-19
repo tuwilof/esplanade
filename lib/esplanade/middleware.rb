3dc11bc9b03010c1dc8ffc38068062f92acab20c
d.efimov <d.efimov@fun-box.ru>
2017-09-19 17:24:47 +0400

    simplify middleware

--- lib/esplanade/middleware.rb
+++ lib/esplanade/middleware.rb
@@ -2,9 +2,6 @@ require 'yaml'
 
 module Esplanade
   class Middleware
-    class Exit < RuntimeError; end
-    class ResponseNotDoc < RuntimeError; end
-
     def initialize(app)
       @app = app
       @tomogram = Tomograph::Tomogram.new(
@@ -15,23 +12,9 @@ module Esplanade
     end
 
     def call(env)
-      valid!(env)
-    rescue Request::NotDocumented
-      Request::Error.not_documented
-    rescue Response::NotDocumented
-      Response::Error.not_documented
-    rescue Request::Unsuitable => e
-      Request::Error.unsuitable(e.message)
-    rescue Response::Unsuitable => e
-      Response::Error.unsuitable(e.message)
-    end
-
-    def valid!(env)
       request = Request.new(env, @tomogram)
-      request.valid! if request.validate?
       status, headers, body = @app.call(env)
       response = Response.new(status, body, request.schema)
-      response.valid! if response.validate?
       [status, headers, body]
     end
   end
