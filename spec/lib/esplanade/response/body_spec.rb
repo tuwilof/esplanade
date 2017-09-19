f72b818349453be3d1731c3fbd75ee3f7c386f3d
d.efimov <d.efimov@fun-box.ru>
2017-09-19 13:29:00 +0400

    cover response body

---
+++ spec/lib/esplanade/response/body_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+RSpec.describe Esplanade::Response::Body do
+  describe '.craft' do
+    subject { described_class.craft(body) }
+
+    context 'empty body' do
+      let(:body) { [] }
+      it { expect(subject).to eq({}) }
+    end
+
+    context 'valid json' do
+      let(:body) { ['{"state": 1}'] }
+      it { expect(subject).to eq('state' => 1) }
+    end
+
+    context 'invalid json' do
+      let(:body) { ['{"state": 1'] }
+      it { expect(subject).to eq('{"state": 1') }
+    end
+  end
+end
